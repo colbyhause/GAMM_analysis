27660a53ac3732f5d5fdfe10de0b0d6860c86e4b
colbyhause <chause@ucdavis.edu>
2019-12-23 09:25:32 -0800

    fixed geomean loop

--- scripts/predict.values.from.GAMMs.R
+++ scripts/predict.values.from.GAMMs.R
@@ -1,6 +1,8 @@
 # predicting out values for chlorophyll. This might be able to allow for all three transect to be in each bin (except for the last ~50 km)
-
+library(tidyverse)
 library(mgcv)
+library(geosphere)
+
 #1. read in csv with all flame data----
 flame_all <- read_csv("data/Data_alltransects_withRKM_FIXED_120919.csv") 
 
@@ -41,6 +43,7 @@ pH$transect <- factor(pH$transect)
 T1 <- flame_all %>% 
   filter(transect == "T1_")
 max(T1$rkm) # for T1 predict out to 111 rkm
+min(T1$rkm)
 
 T2 <- flame_all %>% 
   filter(transect == "T2_")
@@ -70,8 +73,121 @@ newd<- read_csv("data_output/newdataRKM_predict_GAMM.csv")
 
 #2b. New data option 2----
 # Make dataframe to predict across just the rkms that we CURRENTLY have in our dataset
-newd <- data.frame(flame_all[ , c("transect", "rkm")])
-class(newd)
+# Need to get unique rkms to predict across for T1 (stopped earlier than T2 and T3):
+T1_rkms_to_predict_across <- which(flame_all$rkm <= 111.0)
+T1_rkms <- flame_all$rkm[T1_rkms_to_predict_across]
+T1_rkms_u <- data.frame(unique(T1_rkms))
+colnames(T1_rkms_u) <- "unique_rkms"
+max(T1_rkms_u); min(T1_rkms_u)
+
+T2_rkms_to_predict_across <- which(flame_all$rkm <= 145)
+T2_rkms <- flame_all$rkm[T2_rkms_to_predict_across]
+T2_rkms_u <- data.frame(unique(T2_rkms))
+colnames(T2_rkms_u) <- "unique_rkms"
+max(T2_rkms_u); min(T2_rkms_u)
+
+T3_rkms_to_predict_across <- which(flame_all$rkm <= 145)
+T3_rkms <- flame_all$rkm[T3_rkms_to_predict_across]
+T3_rkms_u <- data.frame(unique(T3_rkms))
+colnames(T3_rkms_u) <- "unique_rkms"
+max(T3_rkms_u); min(T3_rkms_u)
+
+all_transect_rkms <- rbind(T1_rkms_u,T2_rkms_u, T3_rkms_u )
+
+# then get the unique rkms from the unique rkms for each transect :
+all_unique_transect_rkms <- unique(all_transect_rkms)
+
+# set rkms for each transect to predict across:
+T1p <- all_unique_transect_rkms %>% 
+  filter (all_unique_transect_rkms >= 0.4 & all_unique_transect_rkms <= 111) # bc T1 started a little later and ended much earlier
+
+T2p <- all_unique_transect_rkms
+
+T3p <- all_unique_transect_rkms
+
+# Need to get unique rkms to predict across for T2 and T3:
+
+new1 <- data.frame(transect = rep(x = "T1_", times = nrow(T1p)), 
+                                  rkm = T1p$unique_rkms)
+new2 <- data.frame(transect = rep(x = "T2_", times = nrow(T2p)), 
+                                  rkm = T2p$unique_rkms)
+new3 <- data.frame(transect = rep(x = "T3_", times = nrow(T3p)), 
+                                  rkm = T3p$unique_rkms)
+                   
+newd <- rbind(new1, new2, new3)
+
+#test <- merge(newd, flame_all, by = "rkm")
+
+flame_ordered_by_rkm <- flame_all[order(flame_all$rkm), ]
+unique_flame_ordered <- unique(flame_ordered_by_rkm[, c("rkm", "lat", "lon")])
+
+rkm_coords_all <- data.frame(flame_all[ , c("rkm", "lon", "lat")]) # make sure class dataframe
+class(rkm_coords_all)
+rkm_coords_all$lon <- as.numeric(as.character(rkm_coords_all$lon)) # since the coords are factors right now, need to turn to a character and then a numeric 
+rkm_coords_all$lat <-  as.numeric(as.character(rkm_coords_all$lat))# # since the coords are factors right now, need to turn to a character and then a numeric
+#check:
+class(rkm_coords_all$lon)
+class(rkm_coords_all$lat)
+
+#Geomean Loop----
+rkms <- all_unique_transect_rkms$unique_rkms
+rkms
+rkm_geomean_df <- NULL # make sure to run this before starting the loop 
+dat <- rkm_coords_all 
+
+for (i in rkms) {
+ print(i)
+  rkm <- subset(dat, dat$rkm == i)
+  coords <- data.frame(rkm[ , c("lon", "lat")])
+  coords$lon <- as.numeric(as.character(coords$lon))
+  coords$lat <- as.numeric(as.character(coords$lat))
+  n <- nrow(rkm)
+  if (nrow(rkm) == 1) {
+    geomean_i$x <- data.frame(mean(coords$lon))
+    geomean_i$y <- data.frame(mean(coords$lat))
+    rkm_geomean_df <- rbind(rkm_geomean_df, data.frame(unique(rkm$rkm), geomean_i, n))
+  } else {
+  geomean_i <- data.frame(geomean(coords))
+  rkm_geomean_df <- rbind(rkm_geomean_df, data.frame(unique(rkm$rkm), geomean_i, n))
+  }
+}
+
+ones <- subset(rkm_geomean_df,  rkm_geomean_df$n == 1)
+test <- subset(rkm_geomean_df, rkm_geomean_df$unique.rkm.rkm. == rkm_geomean_df[1, 1] )
+               
+length(which(rkm_geomean_df$y > 1000))
+max(rkms)
+test <- subset(dat, dat$rkm == rkms[length(rkms)])
+
+test2<- rkm_geomean_df$unique.rkm.rkm. %in% rkms
+test3 <- rkms %in% rkm_geomean_df$unique.rkm.rkm.
+which(test3 == F)
+which(test2 == F)
+rkms[test3 == F, ]
+rkm_geomean_df[test2==F, ]
+
+length(nrow(test))
+# troubleshooting code:
+
+for (i in rkms) {
+  print(i)
+  i <- 49.4890489
+  rkm <- subset(dat, dat$rkm == rkm[1] )
+  coords <- data.frame(dat[ , c("lon", "lat")])
+  coords$lon <- as.numeric(as.character(rkm_coords_all$lon))
+  coords$lat <- as.numeric(as.character(rkm_coords_all$lat))
+  geomean_i <- data.frame(geomean(coords))
+  rkm_geomean_df <- rbind(rkm_geomean_df, data.frame(unique(rkm$rkm), geomean_i))
+}
+
+## just a though- if didnt want to use the geomean, only mybe the first coord, could throw that code in loop instead
+rkm_geomean_df$geomean_lon <- as.numeric(rkm_geomean_df$geomean_lon) # change from list to numeric
+rkm_geomean_df$geomean_lat <- as.numeric(rkm_geomean_df$geomean_lat) # change from list to numeric
+colnames(rkm_geomean_df) <- c("rkm", "geomean_lon", "geomean_lat", "n")
+
+
+test <- merge(rkm_geomean_df, newd, by = "rkm") # should be able to just use this line of code to put the coords back in after you get your predicted data 
+# STOPPED HERE, NEED TO RUN CODE BELOW WITH NEW DATA
 
 
 #3. GAMMS----
@@ -101,6 +217,7 @@ chl_predicted_df <- data.frame(cbind(chl_predicted_df, upper_se, lower_se))
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 #write_csv(chl_predicted_df,"data_output/Predicted_from_GAMM/chl_gam_predicted.05km.csv") # made using newd data from option 1 (#2a)
 write_csv(chl_predicted_df,"data_output/Predicted_from_GAMM/chl_gam_predicted_across_actualrkms.csv")
+chl_predicted_df <- read_csv("data_output/Predicted_from_GAMM/chl_gam_predicted_across_actualrkms.csv")
 
 # plot for visual:
 plot(chl$rkm, chl$CHL, cex = .1)  # raw data 
@@ -135,6 +252,8 @@ turb_predicted_df <- data.frame(cbind(turb_predicted_df, upper_se, lower_se))
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 #write_csv(turb_predicted_df,"data_output/Predicted_from_GAMM/turb_gam_predicted.05km.csv") # made using newd data from option 1 (#2a)
 write_csv(turb_predicted_df,"data_output/Predicted_from_GAMM/turb_gam_predicted_across_actualrkms.csv")
+turb_predicted_df <- read_csv("data_output/Predicted_from_GAMM/turb_gam_predicted_across_actualrkms.csv")
+
 # plot for visual:
 plot(turb$rkm, turb$turb, cex = .1)  # raw data 
 plot(x = turb_predicted_df$rkm, y = turb_predicted_df$fit, cex = .1)
@@ -168,6 +287,7 @@ NO3_predicted_df <- data.frame(cbind(NO3_predicted_df, upper_se, lower_se))
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 #write_csv(NO3_predicted_df,"data_output/Predicted_from_GAMM/NO3_gam_predicted.05km.csv")# made using newd data from option 1 (#2a)
 write_csv(NO3_predicted_df,"data_output/Predicted_from_GAMM/NO3_gam_predicted_across_actualrkms.csv")
+NO3_predicted_df <- read_csv("data_output/Predicted_from_GAMM/NO3_gam_predicted_across_actualrkms.csv")
 
 # plot for visual:
 plot(NO3$rkm, NO3$NO3, cex = .1)  # raw data 
@@ -201,7 +321,7 @@ spCond_predicted_df <- data.frame(cbind(spCond_predicted_df, upper_se, lower_se)
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 #write_csv(spCond_predicted_df,"data_output/Predicted_from_GAMM/spCond_gam_predicted.05km.csv") # made using newd data from option 1 (#2a)
 write_csv(spCond_predicted_df,"data_output/Predicted_from_GAMM/spCond_gam_predicted_across_actualrkms.csv")
-
+spCond_predicted_df <-read_csv("data_output/Predicted_from_GAMM/spCond_gam_predicted_across_actualrkms.csv")
 # plot for visual:
 plot(spCond$rkm, spCond$spCond, cex = .1)  # raw data 
 plot(x = spCond_predicted_df$rkm, y = spCond_predicted_df$fit, cex = .1)
@@ -234,6 +354,7 @@ temp_predicted_df <- data.frame(cbind(temp_predicted_df, upper_se, lower_se))
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 #write_csv(temp_predicted_df,"data_output/Predicted_from_GAMM/temp_gam_predicted.05km.csv") # made using newd data from option 1 (#2a)
 write_csv(temp_predicted_df,"data_output/Predicted_from_GAMM/temp_gam_predicted_across_actualrkms.csv")
+temp_predicted_df <- read_csv("data_output/Predicted_from_GAMM/temp_gam_predicted_across_actualrkms.csv")
 
 # plot for visual:
 plot(temp$rkm, temp$temp, cex = .1)  # raw data 
@@ -268,6 +389,7 @@ pH_predicted_df <- data.frame(cbind(pH_predicted_df, upper_se, lower_se))
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 # write_csv(pH_predicted_df,"data_output/Predicted_from_GAMM/pH_gam_predicted.05km.csv") # made using newd data from option 1 (#2a)
 write_csv(pH_predicted_df,"data_output/Predicted_from_GAMM/pH_gam_predicted_across_actualrkms.csv")
+pH_predicted_df <- read_csv("data_output/Predicted_from_GAMM/pH_gam_predicted_across_actualrkms.csv")
 
 # plot for visual:
 plot(pH$rkm, pH$pH, cex = .1)  # raw data 
@@ -301,6 +423,8 @@ fDOM_predicted_df <- data.frame(cbind(fDOM_predicted_df, upper_se, lower_se))
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 #write_csv(fDOM_predicted_df,"data_output/Predicted_from_GAMM/fDOM_gam_predicted.05km.csv") # made using newd data from option 1 (#2a)
 write_csv(fDOM_predicted_df,"data_output/Predicted_from_GAMM/fDOM_gam_predicted_across_actualrkms.csv")
+fDOM_predicted_df<- read_csv("data_output/Predicted_from_GAMM/fDOM_gam_predicted_across_actualrkms.csv")
+
 # plot for visual:
 plot(fdom$rkm, fdom$fDOM, cex = .1)  # raw data 
 plot(x = fDOM_predicted_df$rkm, y = fDOM_predicted_df$fit, cex = .1)
@@ -333,6 +457,7 @@ DO_predicted_df <- data.frame(cbind(DO_predicted_df, upper_se, lower_se))
 # This dataframe has ONE predicted value per rkm (binned by 0.05 rkm)
 #write_csv(DO_predicted_df,"data_output/Predicted_from_GAMM/DO_gam_predicted.05km.csv") # made using newd data from option 1 (#2a)
 write_csv(DO_predicted_df,"data_output/Predicted_from_GAMM/DO_gam_predicted_across_actualrkms.csv")
+DO_predicted_df<- read_csv("data_output/Predicted_from_GAMM/DO_gam_predicted_across_actualrkms.csv")
 
 # plot for visual:
 plot(DO$rkm, DO$ODO, cex = .1)  # raw data 
@@ -343,9 +468,17 @@ points(DO_predicted_df$rkm, DO_predicted_df$lower_se, cex = .01, col = "red")
 #Combine into 1 data frame:----
 #predicted_gamm_AllParams <- data.frame(cbind(CHL.predicted = chl_predicted_df$fit,turb.predicted =  turb_predicted_df$fit, NO3.predicted = NO3_predicted_df$fit, spCond.predicted = spCond_predicted_df$fit, temp.predicted = temp_predicted_df$fit, pH.predicted = pH_predicted_df$fit, fDOM.predicted = fDOM_predicted_df$fit, DO.predicted = DO_predicted_df$fit, chl_predicted_df[, 1:2])) #### USE IF USED newd DATA FROM 2a
 
-predicted_gamm_AllParams <- data.frame(cbind(CHL.predicted = chl_predicted_df$fit,turb.predicted =  turb_predicted_df$fit, NO3.predicted = NO3_predicted_df$fit, spCond.predicted = spCond_predicted_df$fit, temp.predicted = temp_predicted_df$fit, pH.predicted = pH_predicted_df$fit, fDOM.predicted = fDOM_predicted_df$fit, DO.predicted = DO_predicted_df$fit, data_for_each_param)) ### USE IF USED newd DATA FROM 2b
+predicted_gamm_AllParams <- data.frame(cbind(CHL.predicted = chl_predicted_df$fit,turb.predicted =  turb_predicted_df$fit, NO3.predicted = NO3_predicted_df$fit, spCond.predicted = spCond_predicted_df$fit, temp.predicted = temp_predicted_df$fit, pH.predicted = pH_predicted_df$fit, fDOM.predicted = fDOM_predicted_df$fit, DO.predicted = DO_predicted_df$fit), chl_predicted_df[, 1:2]) ### USE IF USED newd DATA FROM 2b
+
+write_csv(predicted_gamm_AllParams, "data_output/CV/CV_fromPredicted_AllParams_actualRKMS_NOcoordsYet.csv")
+
+# MERGE coords into dataset ----
+# merges the predicted dataframe with geomean dataframe made in step 2b:
+predicted_gamm_AllParamsFinal <- merge(x = predicted_gamm_AllParams, y = rkm_geomean_df, by = "rkm")
 
 #write to csv:
 #write_csv(predicted_gamm_AllParams, "data_output/Predicted_from_GAMM/GAMMPredictedValues_AllParams.csv") ## made using newd data from option 1 (#2a)
 
-write_csv(predicted_gamm_AllParams, "data_output/Predicted_from_GAMM/GAMMPredictedValues_AllParams_across_actualRKMs.csv") ## made using newd data from option 2 (#2b)
+write_csv(predicted_gamm_AllParamsFinal, "data_output/Predicted_from_GAMM/GAMMPredictedValues_AllParams_across_actualRKMs_withGeomeans.csv") ## made using newd data from option 2 (#2b)
+
+# WORKFLOW: now read in this final csv output into the Modeling_Variation_forTimeAnalysis.R script to calculate the coefficients of variation 
